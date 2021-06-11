664fb376bfda344117a17222d00545a45d7f0535
bmanzanoqa <trizmanz@hotmail.com>
2021-06-11 15:03:56 +0000

    BM: Add rolling update

--- service4/app.py
+++ service4/app.py
@@ -10,15 +10,14 @@ app = Flask(__name__)
 @app.route('/get_fortune/<day>/<int:number>', methods=['POST'])
 def get_fortune(day, number):
     if number == 0:
-        return f"On {day} you will loose your house and fiance, it's a tough life!"
+        return f"On {day} YOU WILL LOOSE YOUR HOUSE AND FIANCE, IT'S A TOUGH LIFE!"!"
     elif number <= 3 :
-        return f"On {day}  you will win £300 from the lottery, every little helps!"
+        return f"On {day}  YOU WILL WIN £300 FROM THE LOTTERY, EVERY LITTLE HELPS!"
     elif number <= 6:
-        return f"On {day} you will be invited to countless parties, enjoy!"
+        return f"On {day} YOU WILL BE INVITED TO COUNTLESS PARTIES, ENJOY!"
     elif number <= 9:
-            return f"On {day} you will becomone a millionaire, lucky you!!"
+            return f"On {day} YOU WILL BECOMONE A MILLIONAIRE, LUCKY YOU!!"
     else :
-        return f"On {day} you will pass your assessment with flying colours"
-
+        return f"On {day} YOU WILL PASS YOUR ASSESSMENT WITH FLYING COLOURS"
 
 if __name__ == "__main__": app.run(host="0.0.0.0", port=5000, debug=True)
