6e0d74a7803902ec92473b4bb04ecccf519c8491
bmanzanoqa <bmanzano52@googlemail.com>
2021-06-13 14:20:34 +0000

    BM: Modify jenkins deploy

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,4 +7,6 @@ scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/d
 ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file docker-compose.yaml future2
+    docker rmi -f $(docker images -q)
 EOF
+
